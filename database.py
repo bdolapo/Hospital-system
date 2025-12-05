06c44f2d7512947d928c926118c203c6d294d4e9
Bankole Adedolapo <157981761+bdolapo@users.noreply.github.com>
2025-12-05 15:22:44 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,15 +1,12 @@
-# backend/db.py
-from sqlmodel import SQLModel, create_engine, Session
-import os
+from sqlmodel import SQLModel, Session, create_engine
 
-DB_FILE = os.getenv("HOSPITAL_DB", "hospital.db")
-DATABASE_URL = f"sqlite:///{DB_FILE}"
+DATABASE_URL = "sqlite:///hospital.db"
+engine = create_engine(DATABASE_URL, echo=False)
 
-# NOTE: check_same_thread False is OK for SQLite single process (Streamlit).
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}, echo=False)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
 
 def get_session():
     with Session(engine) as session:
